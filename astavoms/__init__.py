d213a74111e9860fb0602d8e237c0651d9614750
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2016-12-19 15:58:12 +0200

    Bumb version 0.5

--- astavoms/__init__.py
+++ astavoms/__init__.py
@@ -13,4 +13,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.4'
+__version__ = '0.5'
+
